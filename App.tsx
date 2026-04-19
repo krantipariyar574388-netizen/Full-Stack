06ef4838af060c5d7f2f5afa3be718708e2fb3af
krantipariyar574388-netizen <krantipariyar574388@gmail.com>
2026-04-19 14:46:17 +0545

    Add files via upload
    from commit without css

---
+++ App.tsx
@@ -0,0 +1,13 @@
+// Mathi import garda ra tala use garda dubai thau ma Capitalized (FormValidation) garnu
+import FormValidation from './component/formvalidation'; 
+
+function App() {
+  return (
+    <>
+      {/* <formvalidation /> ko satta <FormValidation /> lekhnu */}
+      <FormValidation />
+    </>
+  );
+}
+
+export default App;
